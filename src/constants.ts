680e8e288b37e92fcdba46b49c223ca75dd8e463
AlexanderC <alexander.moldova@gmail.com>
2021-06-15 04:05:21 +0300

    Fix init hash

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import JSBI from 'jsbi'
 
 // @TODO change it to IXS deploy...
-export const FACTORY_ADDRESS = '0x188CE6fD2Ed50dcAc737AB6802b53FC50e757b2f'
+export const FACTORY_ADDRESS = '0x3e764a8d7006476445478B8CD9690FC3547cF3ba'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
 export const INIT_CODE_HASH = '0x77b173365e035eb41ccf1c42c942ce4e2e6da707395a30a933109e236ece6a49'
