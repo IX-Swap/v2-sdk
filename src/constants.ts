7afe5f8cfbc7d8255b528c54f5753d5204b5868c
AlexanderC <alexander.moldova@gmail.com>
2021-07-06 18:12:19 +0300

    Fix periphery tests

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import JSBI from 'jsbi'
 export const FACTORY_ADDRESS = '0x209290A4F4e01affD0AfFc44a11a843534039CF9'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
-export const INIT_CODE_HASH = '0xa14b14aebce2420c0b2a6bbba4e2b66806259f067ce0f7ff2a9632b12afb6916'
+export const INIT_CODE_HASH = '0x964d98bdf7202bc46c540622d0b34dc128bccb606c2e39cc30952c8b51416217'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
