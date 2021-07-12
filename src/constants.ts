8c9a6bd60477a0aa59182aad34b398e8dee19da8
AlexanderC <alexander.moldova@gmail.com>
2021-07-12 19:17:36 +0300

    Refactor the way factory oracle operates

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import JSBI from 'jsbi'
 export const FACTORY_ADDRESS = '0x209290A4F4e01affD0AfFc44a11a843534039CF9'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
-export const INIT_CODE_HASH = '0x964d98bdf7202bc46c540622d0b34dc128bccb606c2e39cc30952c8b51416217'
+export const INIT_CODE_HASH = '0x933158ad30a799504a7514a8d73f7b56b384bf63623d939d7a2961a677210d98'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
