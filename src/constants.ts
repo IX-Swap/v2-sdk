0c70a9d9c94b61530a37eca9f31db17579bd2f46
AlexanderC <alexander.moldova@gmail.com>
2021-07-19 09:40:32 +0300

    Test updates to match core codebase

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import JSBI from 'jsbi'
 export const FACTORY_ADDRESS = '0x209290A4F4e01affD0AfFc44a11a843534039CF9'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
-export const INIT_CODE_HASH = '0x933158ad30a799504a7514a8d73f7b56b384bf63623d939d7a2961a677210d98'
+export const INIT_CODE_HASH = '0x7ee9291a0e1924070b131d5ab24a985787e8f5958284a541fd19c8c365755a79'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
