18adc1dd931e44624da9b679bb5618af3400b205
AlexanderC <alexander.moldova@gmail.com>
2021-08-17 11:56:27 +0300

    Fix multihop swap

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 import JSBI from 'jsbi'
 
 // @TODO change it to IXS deploy...
-export const FACTORY_ADDRESS = '0x85Af0Af2a25392641d477339CE9c3C26845BF271'
+export const FACTORY_ADDRESS = '0xFD5978fc22F976bADDC80778Dc58a8e0f0F6dbb5'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
-export const INIT_CODE_HASH = '0x39dc286d0caad217e8a8d19b90b20758bdfc6adf57e11ce69baa497b7dabebb5'
+export const INIT_CODE_HASH = '0x8d3fe634af820223f669a0f3909c78d55ce2f09b1ece8b308a52c6ea9e90ae40'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
