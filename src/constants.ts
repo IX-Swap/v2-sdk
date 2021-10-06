03b369c201209ad22e275ba58c8bc340cd132c87
AlexanderC <alexander.moldova@gmail.com>
2021-10-06 08:36:32 +0300

    Fix multihop SEC swap

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 import JSBI from 'jsbi'
 
 // @TODO change it to IXS deploy...
-export const FACTORY_ADDRESS = '0x62539c2d42265ebEA7ad4e515A6f759190d07636'
+export const FACTORY_ADDRESS = '0x08797257C154F06865d18F75Fb90Ee7173073999'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
-export const INIT_CODE_HASH = '0xa64bd11dab3411d091d0fac96b09c548b22bcc3a39e44aeda3166ee8fa2ff0a0'
+export const INIT_CODE_HASH = '0x8fc2ee11d080a1a342cb01d2c07cfff939824f7a50bbaa0542c0168067430a5f'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
