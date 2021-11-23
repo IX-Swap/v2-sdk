8fd03fcf5c6ff2cb8cc287a48b5df890b94549af
AlexanderC <alexander.moldova@gmail.com>
2021-11-23 05:50:59 +0200

    Add mitigation force controls and decouple pair deployment

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import JSBI from 'jsbi'
 export const FACTORY_ADDRESS = '0x08797257C154F06865d18F75Fb90Ee7173073999'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
-export const INIT_CODE_HASH = '0xc1ec1d512183d75c6b7fc423b117278449c0f3996bbf98750e907b69ff427d8d'
+export const INIT_CODE_HASH = '0x347dfc5689d6e8d76849e63ee35c9d3551b01f3a5a00829910e6354dd1247448'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
@@ -13,5 +13,5 @@ export const ZERO = JSBI.BigInt(0)
 export const ONE = JSBI.BigInt(1)
 export const FIVE = JSBI.BigInt(5)
 export const _997 = JSBI.BigInt(997) // 0.3% fee
-export const _990 = JSBI.BigInt(990) // 0.1% fee
+export const _990 = JSBI.BigInt(990) // 1% fee
 export const _1000 = JSBI.BigInt(1000)
--- src/entities/pair.test.ts
+++ src/entities/pair.test.ts
@@ -12,7 +12,7 @@ describe('computePairAddress', () => {
       tokenB
     })
 
-    expect(result).toEqual('0xC490B11c2C608355dAbCE57D7284F9b6e6f818a2')
+    expect(result).toEqual('0x0E2022463BC6E511c3b2d54A52d1BAeAb8390321')
   })
   it('should give same result regardless of token order', () => {
     const USDC = new Token(1, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18, 'USDC', 'USD Coin')
@@ -51,7 +51,7 @@ describe('Pair', () => {
 
   describe('#getAddress', () => {
     it('returns the correct address', () => {
-      expect(Pair.getAddress(USDC, DAI)).toEqual('0xA5975A9cc841953828046de0fe41439AC4b31a13')
+      expect(Pair.getAddress(USDC, DAI)).toEqual('0x3139dcF1E684Ab2051425164aEfAcF090F3269Da')
     })
   })
 
