2e464039a8967060e9531e1de7d677fb8e3fd412
AlexanderC <alexander.moldova@gmail.com>
2021-12-07 02:26:06 +0200

    New kovan deploy

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import JSBI from 'jsbi'
 
 // @TODO change it to IXS deploy...
-export const FACTORY_ADDRESS = '0x08797257C154F06865d18F75Fb90Ee7173073999'
+export const FACTORY_ADDRESS = '0xE83942dC9D244418272E6b7D83271D0BcFC0DB30'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
 export const INIT_CODE_HASH = '0xb5bad4e7a4724d0ac1eba89ffae6664ca1040ebbf827d4d137891051685b4596'
