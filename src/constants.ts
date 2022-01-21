6ddaac1e43018cddb3f854d6fdab8227397d4bab
AlexanderC <alexander.moldova@gmail.com>
2022-01-21 18:32:27 +0200

    SC Fixes

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import JSBI from 'jsbi'
 export const FACTORY_ADDRESS = '0xE83942dC9D244418272E6b7D83271D0BcFC0DB30'
 
 // init code hash, ref: periphery/contracts/libraries/IxsV2Library.sol#31
-export const INIT_CODE_HASH = '0xb5bad4e7a4724d0ac1eba89ffae6664ca1040ebbf827d4d137891051685b4596'
+export const INIT_CODE_HASH = '0x1041b70e515ac87fb2b62ca07d73429c08c150ef73420251a2789ef04460a382'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
